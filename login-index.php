4fc8bea32f8411982316c2f069bec8b953eacfad
CM-Poy <117722333+CM-Poy@users.noreply.github.com>
2022-11-09 21:44:39 +0800

    Add files via upload

---
+++ login-index.php
@@ -0,0 +1,30 @@
+<html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+
+  <body>
+        <h2>Rizal Technological University Attendance Management System</h2>
+        <br>
+        <form action= "" method= "POST">
+
+            <p>Email:
+            <input type= "text" name = "email" placeholder= "email@email.com">
+            <br>
+        
+            <p>Password:
+            <input type= "password" name = "pwd" placeholder= "********">
+            <br>
+            <br>
+            
+            <button type="submit" name = "submitlog" >Login</button>
+
+            <button type="submit" name = "submitlog" >Sign-Up</button>
+
+        </form>
+
+
+  </body>
+</html>
