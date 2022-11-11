3b9003e8fc36ca58e858f8f55c982a2b23085748
poy <clintmariusquindao19@gmail.com>
2022-11-11 16:10:46 +0800

    changed html to php file type

---
+++ html/auth-email.php
@@ -0,0 +1,135 @@
+<html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <style type="text/css">
+    #bg{
+    background-image: url("loginregbg.png");
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    }
+
+    #card{ 
+      margin-left: 800px;
+      margin-top: -273px;
+      padding-right: -500px;
+
+     
+    }
+    .univ{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 40px;
+      padding-left: 20px;
+      margin-top: -20px;
+     
+    }
+    .univ2{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 40px;
+      padding-left: 460px;
+      margin-top:-95px;
+     
+    }
+     .twofa{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 20px;
+      padding-left: 480px;
+      margin-top: -30px}
+      
+
+      html{
+        overflow-x: hidden;
+      }
+      #logo{
+        padding-top: 330px;
+        display: flex;
+        align-items: center;
+        margin-left: 270px;
+
+      }
+      .cardcolor{
+        background-color: #194F90;
+        size: 50px;
+      }
+      .reghere{
+        color: #DAA520;
+      }
+      .forg{
+        color: white;
+        margin-top: px;
+        color: red;
+        margin-left: 250px;
+
+
+      }
+      .spacing{
+        margin-top: -20px;
+        margin-left: -30px;
+        margin-right: -30px;
+        margin-bottom: -20px;
+      }
+      .spacing2{
+        margin-top: 10px;
+        margin-bottom: 10px;
+      }
+      label{
+        margin-bottom: 0px;
+      }
+      .btn{
+        margin-top: 10px;
+        margin-bottom: 10px;
+      }
+      .phone{
+        margin-left: 170px;
+        padding-right: -50px;
+        padding-top: 40px;
+      }
+
+
+  </style>
+
+  <body>
+
+        <section id="bg" class=" gradient-custom">
+          <div id="logo">
+           <img src="rtu_logo.png"> <p class="univ text-white">  Rizal Technological University</p></div>
+            <p class="univ2 text-white"> Attendance Management System</p>
+            <p class="twofa text-white">With Two-Factor Authentication</p>
+          
+  <div class="container py-5 h-70">
+    <div  class="">
+      <div id="card" class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="cardcolor text-white" style="border-radius: 1rem; height:330px; padding-top: -150px;">
+          <div class="card-body p-5">
+              
+      
+           
+              <div class="spacing2">
+                <label class="form-label">CODE HAS BEEN SENT TO:</label>
+                <center><p><a class="forg" href="#!"><b>RESEND</a></p></center></b>
+                <input type="CODE" class="form-control form-control-lg" />
+              </div>
+
+              
+              <center><button class="btn btn-outline-light btn-lg px-5" type="submit"><a href="index.html">CONFIRM</button></center>
+              </a>
+              <div class="phone">
+              <p><a href="#!"><b>USE PHONE NUMBER</a></p></b></div>
+              
+              
+
+        
+ </div>
+</div>
+</div>
+</section>
+
+  </body>
+</html>
---
+++ html/login-index.php
@@ -0,0 +1,126 @@
+<html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <title>Pornhub</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <style type="text/css">
+    #bg{
+    background-image: url("loginregbg.png");
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    }
+
+    #card{ 
+      margin-left: 770px;
+      margin-top: -270px;
+      padding-right: -500px;
+
+     
+    }
+    .univ{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 40px;
+      padding-left: 50px;
+      margin-top: -20px;
+      margin-left: -40px;
+     
+    }
+    .univ2{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 40px;
+      padding-left: 430px;
+      margin-top:-95px;
+     
+    }
+     .twofa{
+      text-shadow: 2px 2px black;
+      font-family: Sans-serif;
+      font-size: 20px;
+      padding-left: 432px;
+      margin-top: -30px}
+      
+
+      html{
+        overflow-x: hidden;
+      }
+      #logo{
+        padding-top: 330px;
+        display: flex;
+        align-items: center;
+        margin-left: 250px;
+
+
+      }
+      .cardcolor{
+        background-color: #194F90;
+        size: 50px;
+      }
+      .reghere{
+        color: #DAA520;
+      }
+      .forg{
+        color: white;
+        padding-top: 20px;
+
+      }
+      .spacing{
+        margin-top: -30px;
+        margin-left: -30px;
+        margin-right: -30px;
+        margin-bottom: -20px;
+      }
+      .spacing2{
+        margin-top: -20px;
+        margin-bottom: 10px;
+      }
+
+
+  </style>
+
+  <body>
+
+        <section id="bg" class=" gradient-custom">
+          <div id="logo">
+           <img src="rtu_logo.png"> <p class="univ text-white">  Rizal Technological University</p></div>
+            <p class="univ2 text-white"> Attendance Management System</p>
+            <p class="twofa text-white">With Two-Factor Authentication</p>
+          
+  <div class="container py-5 h-70">
+    <div  class="">
+      <div id="card" class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="cardcolor text-white" style="border-radius: 1rem; height:330px; padding-top: -100px;">
+          <div class="card-body p-5">
+
+
+            <div class="spacing">
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typeEmailX">EMAIL</label>
+                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+
+              </div>
+
+              <div class="spacing2">
+                <label class="form-label" for="typePasswordX">PASSWORD</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+              </div>
+
+              <center><button class="btn btn-outline-light btn-lg px-5" type="submit"><a href="code-index.html">Login</button></center></a>
+              <center><p><a class="forg" href="#!">Forgot password?</a></p></center>
+
+            <div>
+              <center><p class="mb-0">Don't have an account Yet? <a href="signup-index.html" class="reghere">Register Here</a></center>
+              </p>
+            </div>
+ </div>
+</div>
+</div>
+</section>
+
+  </body>
+</html>
