16dfa3b0bc2ee6f54436fc3993218352e8dbea47
R0su <2019-104574@rtu.edu.ph>
2022-11-11 21:52:30 +0800

    made table for add class, made html and css for calendar

--- html/dash-manage.php
+++ html/dash-manage.php
@@ -99,24 +99,48 @@
 			</div>		
 				
 
-		</header>
+                
+	<section>
+    <div class="container-fluid">
+    <div class="col-md-12 text-center">
+    <div class="whole">
+    <div class="card text-white" style="height: 720px; margin-top: 50px; width: 1500px; margin-left: 90px;background-color: #194F90;z-index: 1;position: relative;">
+    <div>
+    <table class="table text-white">
+    <thead class="thead-light">
+    <tr>
+      <th scope="col">CLASS</th>
+      <th scope="col">CODE</th>
+      <th scope="col">DAY</th>
+      <th scope="col">TIME</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody class="tablebody">
+    <tr>
+      <td>INTRODUCTION TO COMPUTING</th>
+      <td>ITP-1111</td>
+      <td>MONDAY</td>
+      <td>7:30AM-9:00AM</td>
+      <td><button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg></button>
+      </td>
+  </tbody>
+</table>
+        					</div>
+        					<div>
+        					<button type="button" class="btn btn-light" style="margin-top: 550px;margin-left: 1360px;">ADD</button>
+        					</div>
+ 
+  </div>
+</div>
+           
 
-		<main>
-			
-			 <div class="container">
-        		<div class="col-md-12 text-center">
-      
-            <a class="index" href="manage.html"><button type="button" class="btn btn-outline-primary">ADD</button>
-            </a>
-           <button type="button" class="btn btn-outline-warning">EDIT</button>
-             </a>
-        	</div>
-    	</div>
 
 
 
-
-		</main>
+		</section>
 
 		<footer>
 
@@ -133,11 +157,10 @@
 
 
 
-
-	</body>
+</header>
+</body>
 
 
 
 
 <html>
-
