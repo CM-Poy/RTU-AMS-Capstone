75601a61f057638a91457837b0dc52b50f9d9156
poy <clintmariusquindao19@gmail.com>
2022-11-14 04:16:23 +0800

    mm

---
+++ html/php/includes/function.php
@@ -0,0 +1,68 @@
+<?php
+require_once 'config.php';
+require_once 'action.php';
+
+
+function signup($fname,$lname,$schoolid,$cnum,$email,$pwd){
+    
+    global $conn;
+
+    $sql = "INSERT INTO users (fname, lname, schoolid, cnumber, email, pwd) VALUES (?, ?, ?, ?, ?, ?)";
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    try {
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$fname,$lname,$schoolid,$cnum,$email,$hashedPwd]);
+        header("location: ../signup-index.php?signup=successful");
+    } catch (PDOException $e){
+        echo '<span style="color:red;text-align:center;">' .$sql. '</span>' .$e->getMessage();
+    }
+
+}
+
+
+
+
+function login($email,$pwd){
+    global $conn;
+    $sql= "SELECT * FROM users WHERE users_email =:email";
+
+    try{
+        $stmt = $conn->prepare($sql);
+        $statement = $conn->prepare($sql);
+        $stmt->execute(array(':email'=>$email));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $pwdHashed = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $checkPwd = password_verify($pwd, $pwdHashed[0]['users_pwd']);
+
+        if($stmt->rowCount() > 0){
+
+            if($email == $row['users_email']){
+
+                if($checkPwd){
+                    header("location: ../login-index.php?error=none");
+                }else{
+                    header("location: ../login-index.php?error=none");
+                }
+               
+
+            }else{
+                header("location: ../login-index.php?error=loginfailed2");
+    
+            }
+
+        }else{
+            header("location: ../login-index.php?error=loginfailed3");
+        }
+        
+        
+    }catch (PDOException $e){
+        echo '<span style="color:red;text-align:center;">' .$sql. '</span>' .$e->getMessage();
+    }
+
+    
+    
+}
+   
+?>
