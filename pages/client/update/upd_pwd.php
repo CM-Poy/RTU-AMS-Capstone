9bf0176b276909a8412a33040e2efe972925e3d4
jaysusq <117895242+jaysusq@users.noreply.github.com>
2023-05-06 14:49:51 +0800

    changes

--- pages/client/update/upd_pwd.php
+++ pages/client/update/upd_pwd.php
@@ -46,6 +46,7 @@ if(isset($_POST['btnUpd'])){
   
 <head>
 <meta charset="UTF-8">
+    <link rel='icon' href='../../../images/rtu-logo.png'/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="../../../css/css_update/updstyle.css">
     
--- pages/superadmin/update/upd_rooms.php
+++ pages/superadmin/update/upd_rooms.php
@@ -39,6 +39,7 @@ $result->execute([$id]);
   
 <head>
     <meta charset="UTF-8">
+    <link rel='icon' href='../../../images/rtu-logo.png'/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../css/css_update/updstyle.css">
     
